4e3de433d5651f8c1cc2fd155518610bcaff3a38
devzx77 <104359838+devzx77@users.noreply.github.com>
2022-04-25 18:57:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,24 @@ var express=require('express')
 const app=express()
 
 app.get('/',(req,res)=>{
-     res.send("Hello world!")
+    var name=req.query.name
+    var dept=req.query.dept
+    var company=req.query.company
+if(name && dept && company){
+        res.send(name +" working in "+dept+ " department at " +company)
+    }
+ else if(name && !company && !dept) {
+     res.send("Hello " + name)
+ }  
+    else{ res.send("Hello world!")
+}
 })
 
-app.get('/home/:id',(req,res)=>{
-    const id=req.params.id
-    res.send("This is a Home page"+ id)
+app.get('/home',(req,res)=>{
+     res.send("This is a Home page")
 })
 app.get('/about',(req,res)=>{
-
-      const id=req.query.id
-      res.send("This page contains the details of About Company!"+id)
+  res.send("This page contains the details of About Company!")
 })
 app.get('/contact',(req,res)=>{
     res.send('this is a contact page')
@@ -21,7 +28,11 @@ app.get('/contact',(req,res)=>{
 app.get('/address',(req,res)=>{
     res.send('this page is directd to adress of the company')
 })
+app.get('/',(req,res)=>{
+    var name=req.query.name
+    res.send('Hello '+ name +'!')
+})
 app.use((req,res,next)=>{
     res.status(404).send('Error 404 page not found!')
 })
-app.listen(9000,'127.0.0.1')
+app.listen(9000)
