6bbf990b547015824b0e40c7d343f84e0ca99e24
devzx77 <devaraj.basappa@softsuave.com>
2022-04-25 19:14:29 +0530

    Updated the Name.

--- name.js
+++ name.js
@@ -2,6 +2,6 @@ var express=require('express')
 const app=express()
 app.get('/:name',(req,res)=>{
     var name=req.params.name
-    res.send('Hello '+ name+'!')
+    res.send('Hello this is Dev '+ name+'!')
 })
 app.listen(3000)
