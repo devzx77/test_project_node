a2e78632018af739da83978e39276deef7cfbdb5
devzx77 <devaraj.basappa@softsuave.com>
2022-04-26 18:54:26 +0530

    updated post method

--- name.js
+++ name.js
@@ -1,12 +1,17 @@
 const express=require('express')
 const fs=require('fs')
 const app=express()
+const bodyparser=require('body-parser')
+app.use(bodyparser.urlencoded({extended:false}))
+app.use(bodyparser.json())
 
 
 app.get('/',(req,res)=>{
-       res.send('Add user details in the URL')
+       res.send('Add user details in the Test URL')
 })
-app.get('/api/members',(req,res)=>{
+
+/*using get method
+app.get('/adduser',(req,res)=>{
   const  id=req.query.id;
   const name=req.query.name;
   const std=req.query.std;
@@ -23,9 +28,31 @@ app.get('/api/members',(req,res)=>{
     const datas=fs.readFileSync('details.txt','utf8',(err,data)=>{})
       res.send(datas)
   }
+})*/
+//using post method to create or insert data
+app.post('/adduser',(req,res)=>{
+  const id=req.body.id;
+  const name=req.body.name;
+  const std=req.body.std;
+
+  const add={
+    id:id,
+    name:name,
+    std:std
+  }
+   
+  if(name && id && std){
+      fs.appendFileSync('details.txt',JSON.stringify(add),(err)=>{})
+      const datas=fs.readFileSync('details.txt','utf8',(err,data)=>{})
+      res.send(datas);
+  }
+  else{
+    const datas=fs.readFileSync('details.txt','utf8',(err,data)=>{})
+      res.send(datas)
+  }
 })
 app.get('*',(req,res)=>{
     res.status(404).send('Error page not found')
 })
 
-app.listen(8080)
+app.listen(8000)
